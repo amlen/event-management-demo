6358c98d33e0668123e260585cab7c7bdfaee9f5
Akharaze <yassine.akharaze@capgemini.com>
2021-04-26 02:35:16 +0200

    Create project structure

---
+++ src/main/java/com/example/eventmanagementdemo/EventManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagementdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventmanagementdemo/EventManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagementdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
